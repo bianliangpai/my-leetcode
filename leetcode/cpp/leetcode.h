5ed65b8099b4d60cb4f680709214dacb231a2409
bianliangpai <bianliangpai@gmail.com>
2022-08-13 16:22:36 +0800

    NEW: solved 004

--- leetcode/cpp/leetcode.h
+++ leetcode/cpp/leetcode.h
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <array>
+#include <cmath>
 #include <deque>
 #include <forward_list>
 #include <initializer_list>
@@ -12,11 +13,13 @@
 #include <map>
 #include <memory>
 #include <numeric>
+#include <queue>
 #include <set>
 #include <stack>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 #endif  // !LEETCODE
